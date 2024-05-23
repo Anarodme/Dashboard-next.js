34f4636e050b55f336f88a7197f5646da38c9201
Anarodme <109930993+Anarodme@users.noreply.github.com>
2024-05-23 14:56:41 -0600

    New commit

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -25,7 +25,6 @@ export default function EditInvoiceForm({
  
   return  (
   <form action={dispatch}>
-    // Passing an id as argument won't work
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         <div className="mb-4">
           <label htmlFor="customer" className="mb-2 block text-sm font-medium">
@@ -51,7 +50,6 @@ export default function EditInvoiceForm({
           </div>
         </div>
 
-        {/* Invoice Amount */}
         <div className="mb-4">
           <label htmlFor="amount" className="mb-2 block text-sm font-medium">
             Choose an amount
